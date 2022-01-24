57cbf2fe806f8c623ee3e1a55438d134f920b21a
Thibault BALSAMO <thibma@MacBook-Pro-de-Thibault.local>
2022-01-24 10:35:42 +0000

    first commit

---
+++ ARCoins/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARCoins
+//
+//  Created by Thibault BALSAMO on 24/01/2022.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
